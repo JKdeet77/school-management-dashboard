90bc4f9168559a4b3254d3abe9841524f2d66ad4
JKDeet77 <parisperez989@gmail.com>
2026-05-21 16:53:34 -0500

    Created form inputs for TeachersListPage

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,5 +1,6 @@
 import Announcements from '@/components/Announcements'
 import BigCalendar from '@/components/BigCalendar'
+import FormModal from '@/components/FormModal'
 import Performance from '@/components/Performance'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -24,7 +25,23 @@ const SingleTeacherPage = () => {
               />
             </div>
             <div className="w-2/3 flex flex-col justify-between gap-4">
-              <h1 className='text-xl font-semibold'>Leonard Snyder</h1>
+              <div className='flex items-center gap-4'>
+                <h1 className='text-xl font-semibold'>Leonard Snyder</h1>
+                <FormModal table='teacher' type='update' data={{
+                  id: 1,
+                  username: "leonardsnyder",
+                  email: "leonardsnyder@jfisd.org",
+                  password: "password",
+                  firstName: "Leonard",
+                  lastName: "Snyder",
+                  phone: "+1 234 567 8900",
+                  address: "1234 Fake St., Anytown, MO, US",
+                  bloodType: "A+",
+                  dateOfBirth: "1982-12-15",
+                  sex: "Male",
+                  img: "https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1200",
+                }} />
+              </div>
               <p className='text-sm text-gray-500'>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
               <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
